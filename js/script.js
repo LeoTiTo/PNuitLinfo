5b64bdbcd61f70d1f522eaae1b47446276e00a8c
LeoTiTo <lochuutran12@gmail.com>
2025-03-11 09:33:53 +0100

    new Commit & Push

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let text = document.getElementById('text');
+let bird1 = document.getElementById('bird1');
+let bird2 = document.getElementById('bird2');
+let water = document.getElementById('water');
+let header = document.getElementById('header');
+let wave1 = document.getElementById('wave1');
+let wave2 = document.getElementById('wave2');
+let wave3 = document.getElementById('wave3');
+let wave4 = document.getElementById('wave4');
+
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+
+    text.style.top = 50 + value * -0.5 + '%';
+    bird1.style.top = value * -1.5 + 'px';
+    bird1.style.left = value * 2 + 'px';
+    bird2.style.top = value * -1.5 + 'px';
+    bird2.style.left = value * -5 + 'px';
+    water.style.top = value * -0.12 + 'px';
+    header.style.top = value * 0.5 + 'px'
+    wave1.style.backgroundPositionX = 400 + value * 4 + 'px';
+    wave2.style.backgroundPositionX = 300 + value * -4 + 'px';
+    wave3.style.backgroundPositionX = 200 + value * 2 + 'px';
+    wave4.style.backgroundPositionX = 100 + value * -2 + 'px';
+})
